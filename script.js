d75017d7d7971fbfb94de52fe2522dd46cf790fa
Sam Shacter <sshacter@gmail.com>
2024-05-03 10:14:41 -0500

    completed step one

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ onReady();
 function onReady() {
   console.log('Javascript is working!');
 }
+
+
+function makeFire() {
+console.log('make fire works')
+let emojis = document.getElementById('emojis')
+emojis.innerHTML += "🔥"
+}
+
+function makeIce() {
+  console.log('make ice works')
+  let emojis = document.getElementById('emojis')
+emojis.innerHTML += "❄️"
+}
